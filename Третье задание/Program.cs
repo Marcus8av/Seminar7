872f8129ba2b455329149edb6749cac08c83fe5f
markus b <mnogo_bukof@mail.ru>
2022-11-21 17:39:13 +0700

    третье задание

---
+++ Третье задание/Program.cs
@@ -0,0 +1,40 @@
+﻿/* Запросите от пользователя размерности двумерного массива. 
+Напишите метод для заполнения массива случайными числами. 
+Напишите метод для поиска ср. арифметического значения каждого столбца. */
+
+void FillArrayRandom(double[,] array) 
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+            Console.Write(array[i, j] + ";");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
+
+void Arithmetic(double[,] array)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        double numm = 0; 
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            numm = (numm + array[i, j]);
+        }
+        Console.Write($"Среднее арифмитическое каждого столбца = {numm / array.GetLength(0):f1}"+ "; ");
+    }
+}
+System.Console.WriteLine("Введите m:");
+int m = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите n:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double[,] array = new double[n, m]; 
+FillArrayRandom(array);
+Arithmetic(array);
