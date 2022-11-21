61aa8947742e0746a0c27cef9b6069b0634a5479
markus b <mnogo_bukof@mail.ru>
2022-11-21 17:37:43 +0700

    первое задание

---
+++ Первое задание/Program.cs
@@ -0,0 +1,30 @@
+﻿/* Запросите от пользователя размерности двумерного массива. 
+Напишите метод для заполнения массива случайными числами. 
+Напишите метод для вывода массива на экран (постарайтесь вывести массив красиво). 
+Округлите значения, генерируемые Random до двух знаков после запятой. */
+
+void FillArray(double[,] array, int m, int n)
+{
+    for (int i = 0; i < m; i++)
+{
+        Console.Write("[");
+        for (int j = 0; j < n; j++)
+{
+            array[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+            Console.Write(Math.Round(array[i, j], 2) + ";");
+}
+        Console.Write("]");
+        Console.WriteLine(".");
+}
+}
+
+System.Console.WriteLine($"Введите размерность двумерного массива");
+
+System.Console.WriteLine("Введите m:");
+int m = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите n:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double[,] array = new double[m, n];
+FillArray(array,m,n);
